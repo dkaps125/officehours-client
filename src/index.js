b7324e1778158ca0e8022afe2da5d00bb1ac456e
Greg Bekher <greg@bekher.me>
2018-08-20 23:55:37 -0400

    add toastr config

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import toastr from 'toastr';
 //import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
@@ -7,5 +8,7 @@ import $ from 'jquery';
 window.jQuery = window.$ = $;
 const bootstrap = require('bootstrap');
 
+toastr.options.preventDuplicates = true;
+toastr.options.positionClass = 'toast-bottom-right';
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
