7af5826c96ac902b9af119098eefb01d1d3a5f86
Greg Bekher <greg@bekher.me>
2018-08-25 14:30:47 -0400

    modernize user edit

--- src/components/UserDetails/UserEdit.jsx
+++ src/components/UserDetails/UserEdit.jsx
@@ -5,27 +5,22 @@ class UserEdit extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      role: props.user.role,
+      // role: props.user.role,
       name: props.user.name,
       directoryID: props.user.directoryID
     };
-
   }
 
-  componentDidMount() {
+  componentDidMount() {}
 
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-
-  }
+  componentDidUpdate(prevProps, prevState) {}
 
   toastAndUpdate = (msg, cb) => {
     toastr.success(msg);
     cb();
-  }
+  };
 
-  handleInputChange = (event) => {
+  handleInputChange = event => {
     const target = event.target;
     const value = target.type === 'checkbox' ? target.checked : target.value;
     const name = target.name;
@@ -33,42 +28,55 @@ class UserEdit extends React.Component {
     this.setState({
       [name]: value
     });
-  }
+  };
 
-  handleSubmit = (event) => {
+  handleSubmit = event => {
     this.props.handleSubmit(this.props.user, this.state);
-  }
+  };
 
   render() {
-    return <div>
-      <h3>Edit user</h3>
-      <form className="form-inline">
-        <div className="form-group">
-          <label htmlFor="userName">&nbsp;Name</label>
-          <input type="text" className="form-control" name="name"
-            value={this.state.name}
-            onChange={this.handleInputChange} />
-        </div>
-        <div className="form-group">
-          <label htmlFor="directoryID">&nbsp;Directory ID</label>
-          <input type="text" className="form-control" name="directoryID"
-            value={this.state.directoryID}
-            onChange={this.handleInputChange} />
-        </div>
-        <div className="form-group">
-          <label htmlFor="role">&nbsp;Role</label>
-          <select className="form-control" name="role"
-            onChange={this.handleInputChange} value={this.state.role}>
-            <option value="Student">Student</option>
-            <option value="TA">TA</option>
-            <option value="Instructor">Instructor</option>
-          </select>
-        </div>
-        &nbsp;
-        <button type="button" className="btn btn-default"
-          onClick={this.handleSubmit}>Update user</button>
-      </form>
-    </div>
+    return (
+      <div>
+        <h3>Edit user</h3>
+        <form className="form-inline">
+          <div className="form-group">
+            <label htmlFor="userName">&nbsp;Name</label>
+            <input
+              type="text"
+              className="form-control"
+              name="name"
+              value={this.state.name}
+              onChange={this.handleInputChange}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="directoryID">&nbsp;Directory ID</label>
+            <input
+              type="text"
+              className="form-control"
+              name="directoryID"
+              value={this.state.directoryID}
+              onChange={this.handleInputChange}
+            />
+          </div>
+          {/* legacy:
+          <div className="form-group">
+            <label htmlFor="role">&nbsp;Role</label>
+            <select className="form-control" name="role"
+              onChange={this.handleInputChange} value={this.state.role}>
+              <option value="Student">Student</option>
+              <option value="TA">TA</option>
+              <option value="Instructor">Instructor</option>
+            </select>
+          </div>
+          */}
+          &nbsp;
+          <button type="button" className="btn btn-default" onClick={this.handleSubmit}>
+            Update user
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
